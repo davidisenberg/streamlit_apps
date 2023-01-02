4a5601ab7167c040208f6610956284aedc0aca9b
davidisenberg <30781967+davidisenberg@users.noreply.github.com>
2023-01-02 11:14:07 -0500

    add try except

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,13 +28,18 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -47,3 +52,5 @@ streamlit.dataframe(my_data_rows)
 streamlit.header("Add a fruit to the list")
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', add_my_fruit)
+
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
